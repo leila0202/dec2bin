782689e525f3197b90ac353a361406945a22cfbe
Leila Zrim <leilazrim@gmail.com>
2024-01-17 14:06:12 +0100

    [js - 08] [Step 06] Get the "e" event object and log it

--- script.js
+++ script.js
@@ -8,4 +8,6 @@ const checkUserInput = () => {
 
 convertBtn.addEventListener("click", checkUserInput);
 
-numberInput.addEventListener("keydown", () => {});
+numberInput.addEventListener("keydown", (e) => {
+  console.log(e);
+});
