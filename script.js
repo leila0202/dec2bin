0888c053ae0413b2f12d9248c8fd4ecd34958313
Leila Zrim <leilazrim@gmail.com>
2024-01-17 14:46:10 +0100

    [js - 08] [Step 14] Short circuit function

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const result = document.getElementById("result");
 const checkUserInput = () => {
   if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
     alert("Please provide a decimal number");
+    return;
   }
   console.log(numberInput.value);
 };
