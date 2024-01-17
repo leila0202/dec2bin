26d46aa29d2a10c9e074fd349bef0c0bb5e3b896
Leila Zrim <leilazrim@gmail.com>
2024-01-17 13:56:41 +0100

    [js - 07] [Step 03] Add logging for testing purposes

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const checkUserInput = () => {};
+const checkUserInput = () => {
+  console.log(numberInput.value);
+};
