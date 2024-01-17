f4f5a5a226d0d1b06b06c732b2960a72a697d310
Leila Zrim <leilazrim@gmail.com>
2024-01-17 15:30:05 +0100

    [js - 08] [Step 29] Add "input" to the "inputs" array

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const decimalToBinary = (input) => {
   while (input > 0) {
     const quotient = Math.floor(input / 2);
     const remainder = input % 2;
+    inputs.push(input);
     input = quotient;
   }
 };
