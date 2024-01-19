e17dfdd32227962386c5e221e8d06846a56ba80e
Leila Zrim <leilazrim@gmail.com>
2024-01-19 09:33:55 +0100

    [js - 08] [Step 31] Log the "inputs" array

--- script.js
+++ script.js
@@ -11,8 +11,12 @@ const decimalToBinary = (input) => {
     const quotient = Math.floor(input / 2);
     const remainder = input % 2;
     inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
     input = quotient;
   }
+
+  console.log("Inputs:", inputs);
 };
 
 const checkUserInput = () => {
