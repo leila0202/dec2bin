3ab226195f8b6e10f7f1349f97e07644de62ad6a
Leila <leilazrim@gmail.com>
2024-02-04 21:57:15 +0100

    [js - 08] [Step 57] Log the number to the console

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const countdown = (number) => {};
+const countdown = (number) => {
+  console.log(number);
+};
 
 const decimalToBinary = (input) => {
   let binary = "";
