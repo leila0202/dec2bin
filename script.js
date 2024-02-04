dc33a8d2476f656e15194da4e93e14a2d8aae425
Leila <leilazrim@gmail.com>
2024-02-04 22:47:22 +0100

    [js - 08] [Step 94] Set id to the "input val"

--- script.js
+++ script.js
@@ -33,7 +33,9 @@ const showAnimation = () => {
   result.innerText = "Call Stack Animation";
   animationData.forEach((obj) => {
     setTimeout(() => {
-      animationContainer.innerHTML += `<p id=""></p>`;
+      animationContainer.innerHTML += `
+      <p id="${obj.inputVal}"></p>
+      `;
     }, obj.addElDelay);
   });
 };
