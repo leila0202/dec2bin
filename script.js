4279d10553785b58a2278c2ddc524c0152e111e2
Leila <leilazrim@gmail.com>
2024-02-04 22:42:47 +0100

    [js - 08] [Step 90] Iterate through the whole "animationdata" array

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ const decimalToBinary = (input) => {
 
 const showAnimation = () => {
   result.innerText = "Call Stack Animation";
+  animationData.forEach((obj) => {});
 };
 
 const checkUserInput = () => {
