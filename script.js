f27ea5ed7249b02759adf70c3b42bcce90c505b0
Leila <leilazrim@gmail.com>
2024-02-04 21:58:20 +0100

    [js - 08] [Step 58] Add base case to the recursive function

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const result = document.getElementById("result");
 
 const countdown = (number) => {
   console.log(number);
+  if (number === 0) {
+    return;
+  }
 };
 
 const decimalToBinary = (input) => {
