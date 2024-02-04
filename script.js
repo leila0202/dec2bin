99ade93c6bcd38c624a017ba462c41c3ecf94c8a
Leila <leilazrim@gmail.com>
2024-02-04 22:38:59 +0100

    [js - 08] [Step 87] Get the animation container

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+const animationContainer = document.getElementById("animation-container");
 
 const animationData = [
   {
