2f2cf82a4c01dbfaab19bbfb1f36874970908cde
Leila <leilazrim@gmail.com>
2024-02-04 21:53:21 +0100

    [js - 08] [Step 53] Simulate callStack by manipulating the array

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const callStack = [
-  'a(): returns "freeCodeCamp " + b()',
-  'b(): returns "is " + "awesome!"',
-];
+const callStack = ['a(): returns "freeCodeCamp " + "is awesome!"'];
 
 const a = () => {
   return "freeCodeCamp" + b();
