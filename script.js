50ba3f27e65598fd1034893772461826c878268f
Leila <leilazrim@gmail.com>
2024-02-04 22:33:59 +0100

    [js - 08] [Step 83] Add empty array for storing the Animations

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const animationData = [];
+
 const decimalToBinary = (input) => {
   if (input === 0 || input === 1) {
     return String(input);
