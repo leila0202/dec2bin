06bf59830deed1f63e5c350970d0f6836baccada
Leila <leilazrim@gmail.com>
2024-02-04 22:29:39 +0100

    [js - 08] [Step 80] Use "setTimeOut"

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ const decimalToBinary = (input) => {
 
 const showAnimation = () => {
   console.log("free");
-  console.log("Code");
+  setTimeout(() => {
+    console.log("Code");
+  }, 1000);
   console.log("Camp");
 };
 
